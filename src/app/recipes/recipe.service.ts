f4a253dba7c82295a78ce74d15b4cff1ab324132
sampanna pokhrel <sampanna99@gmail.com>
2018-07-05 19:15:03 -0400

    took eventemmiter out and added Subject

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,10 @@
+import {Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
   new Recipe('A Test Recipe', 'This is simply the test', 'https://hips.hearstapps.com/del.h-cdn.co/assets/17/34/2048x1024/landscape-1503418862-chicken-thighs-delish.jpg?resize=768:*', 
   [
