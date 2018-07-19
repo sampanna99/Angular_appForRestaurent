6e9f428de177e59affe9e730466524ca7ea020a9
sampanna pokhrel <sampanna99@gmail.com>
2018-07-18 21:51:08 -0400

    edit

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -13,6 +13,11 @@ startedEditing = new Subject<number>();
   getIngredients(){
       return this.ingredients.slice();
   }
+
+  getIngredient(index: number){
+      return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient){
       this.ingredients.push(ingredient);
       this.ingredientsChanged.next(this.ingredients.slice());
